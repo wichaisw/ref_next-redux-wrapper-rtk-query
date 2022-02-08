64be66b626c3c5fe2d28ce860a5c47d1d233a655
wichaisw <wichai.sw19@gmail.com>
2022-02-08 16:02:31 +0700

    fix: config tailwind to read button variable class

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
           <li className='flex'>logo</li>
           <li className='flex w-6/12 px-2'>
             <input type="text" id='search' className='w-full rounded text-black p-1'/>
-            <Button style={`${buttonStyle.orange}`} onClick={() => alert('search')}>Search</Button>
+            <Button style={buttonStyle.orange} onClick={() => alert('search')}>Search</Button>
           </li>          
         </ul>
       </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/button-style.ts"
   ],
   theme: {
     extend: {},
